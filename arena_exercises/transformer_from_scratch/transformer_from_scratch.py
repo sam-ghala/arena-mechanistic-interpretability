9799a2e5246fbf8259b50441548a1f1517ce84df
sam-ghala <ghalayini.sj@gmail.com>
2026-01-28 21:05:43 +0100

    moved over first two chapter notebooks

---
+++ arena_exercises/transformer_from_scratch/transformer_from_scratch.py
@@ -0,0 +1,40 @@
+"""
+Docstring for arena_exercises.transformer_from_scratch.transformer_from_scratch
+
+following along with the ARENA notebooks and this is my transformer from memory practice, modeled after their architecture 
+"""
+
+# imports
+import einops
+
+# device
+
+# model config
+
+# training config
+
+# embed
+
+# pos embed
+
+# layernorm
+
+# attention
+
+# mlp
+
+# t block
+
+# unembed
+
+# model
+
+# get dataset
+
+# get log probs
+
+# trainer
+
+# sampler
+
+# main
